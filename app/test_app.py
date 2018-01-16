9a7a736623e3651d4ec72822a25f016e51e0ce3a
Moccozet, Sascha A. (IntlAssoc) <saschamoccozet@gmail.com>
2018-01-16 15:11:22 -0500

    Refactor the whole GUI inserting a MyQTableWidget class

--- app/test_app.py
+++ app/test_app.py
@@ -1,7 +1,7 @@
 import sys
-from PyQt5.QtCore import QCoreApplication, Qt
+from PyQt5.QtCore import QCoreApplication, Qt, QSize
 from PyQt5.QtGui import *
-import PyQt5.QtWidgets as qw
+import PyQt5.QtWidgets as Qw
 import csv
 import sip
 sip.setapi('QString', 2)
@@ -11,13 +11,12 @@ from test_skeleton import Ui_MainWindow
 from fuzzywuzzy import process as zz
 
 
-class MyQButtonGroup(qw.QButtonGroup):
+class MyQButtonGroup(Qw.QButtonGroup):
 
-    def __init__(self, layout, btn_checks=None):
-        qw.QButtonGroup.__init__(self)
+    def __init__(self, layout):
+        super(MyQButtonGroup, self).__init__()
         self.setExclusive(False)
         self.layout = layout
-        self.btn_checks=btn_checks
 
     def update_checkboxes(self, tok, matches, df):
         self.clean_checkboxes()
@@ -31,9 +30,10 @@ class MyQButtonGroup(qw.QButtonGroup):
             widg.deleteLater()
 
     def create_checkboxes(self, tok, matches, df):
+
         nbr_widget = self.layout.count()
         for n, (match, score) in enumerate(matches):
-            btn = qw.QCheckBox(f'{n} - ' + match)
+            btn = Qw.QCheckBox(f'{n} - ' + match)
             cond = (df.loc[match, 'alias'] == df.loc[tok, 'alias']) \
                    and (df.loc[match, 'alias'] != '')
             if (match == tok) or cond:
@@ -42,35 +42,82 @@ class MyQButtonGroup(qw.QButtonGroup):
             self.layout.insertWidget(self.layout.count() - nbr_widget, btn)
 
     def set_shortcut(self):
+        """
+        Create the shorcut for all the checkbox
+        """
+
         for n, btn in enumerate(self.buttons()):
             btn.setShortcut(f'Alt+{n}')
 
+class MyQTableWidget(Qw.QTableWidget):
 
-class MyWindow(qw.QMainWindow, Ui_MainWindow):
+    def __init__(self, Qwidget, window):
+        super(MyQTableWidget, self).__init__(Qwidget)
+
+        sizePolicy = Qw.QSizePolicy(Qw.QSizePolicy.Expanding, Qw.QSizePolicy.Expanding)
+        sizePolicy.setHorizontalStretch(0)
+        sizePolicy.setVerticalStretch(0)
+        sizePolicy.setHeightForWidth(self.sizePolicy().hasHeightForWidth())
+
+        self.setSizePolicy(sizePolicy)
+        self.setMaximumSize(QSize(16777215, 16777215))
+        self.setEditTriggers(Qw.QAbstractItemView.EditKeyPressed)
+        self.setTabKeyNavigation(False)
+        self.setDragDropOverwriteMode(False)
+        self.setSelectionMode(Qw.QAbstractItemView.SingleSelection)
+        self.setSelectionBehavior(Qw.QAbstractItemView.SelectRows)
+        self.setObjectName("vocabTableWidget")
+        self.setColumnCount(0)
+        self.setRowCount(0)
+        self.setFocus()
+
+    def print_table(self, df, vocab_limit=1000):
+        temp_df = df.reset_index()
+        nrows, ncols = temp_df.shape
+        self.setColumnCount(ncols)
+        self.setRowCount(min([nrows, vocab_limit]))
+
+        for i in range(self.rowCount()):
+            for j in range(ncols):
+                self.setItem(i, j, Qw.QTableWidgetItem(str(temp_df.iat[i, j])))
+
+        self.resizeColumnsToContents()
+        self.resizeRowsToContents()
+        self.setHorizontalHeaderLabels(temp_df.columns.tolist())
+        self.setSelectionBehavior(Qw.QTableWidget.SelectRows)
+
+
+    # def extract_row_info(self, window):
+    #     items = self.selectedItems()  # selected row
+    #     tok, clf, alias, notes = (str(i.text()) for i in items)
+    #     if alias is not '':
+    #         window.aliasEdit.setText(alias)  # preferred alias
+    #     else:
+    #         window.aliasEdit.setText(tok)  # Default to tok
+    #     # btn = self.clf_mapper[clf]  # which button?
+    #     btn = window.clf_mapper.get(clf, window.unsetButton)
+    #     btn.toggle()  # toggle that button
+    #     window.notesTextEdit.setText(notes)  # show any notes
+    #
+    #     matches = zz.extractBests(tok, window.df.index.tolist(),
+    #                               limit=10, score_cutoff=window.thres)
+    #
+    #     window.vertCheckButtonGroup.update_checkboxes(tok, matches, window.df)
+
+
+class MyWindow(Qw.QMainWindow, Ui_MainWindow):
 
     def __init__(self):
-        qw.QMainWindow.__init__(self)
+        Qw.QMainWindow.__init__(self)
         self.setupUi(self)
-        # self.model = QStandardItemModel(self)
-        # self.vocabTableView.setModel(self.model)
-        # self.vocabTableView.horizontalHeader().setStretchLastSection(True)
-        # self.vocabTableView.selectionChang
-        self.openFile = self.actionOpen
-        self.openFile.setStatusTip('Open File')
-        self.openFile.triggered.connect(self.file_open)
-        self.saveFile = self.actionSave
-        self.saveFile.setStatusTip('Save File')
-        self.saveFile.triggered.connect(self.file_save)
+        self.set_menu()
 
-        self.vocabTableWidget.itemSelectionChanged.connect(self.extract_row_info)
-        # self.vocabTableWidget.itemClicked.connect(self.extract_row_info)
-        # self.vocabTableWidget.keyReleaseEvent(QKeyEvent(Qt.Key_Up)).connect(self.extract_row_info)
+        self.actionExit.triggered.connect(self.close_application)
 
-        # self.keyReleaseEvent()
+        self.vocabTableWidget = MyQTableWidget(self.centralwidget, self)
+        self.gridLayout.addWidget(self.vocabTableWidget, 1, 2, 1, 1)
+        self.vocabTableWidget.itemSelectionChanged.connect(self.table_item_selected)
 
-
-
-        self.vocabTableWidget.setFocus()
         self.simthresSlider.sliderReleased.connect(self.fuzz_thres)
         self.df = None
         self.vocab_limit = 1000
@@ -93,58 +140,35 @@ class MyWindow(qw.QMainWindow, Ui_MainWindow):
             'not yet classified': pd.np.nan
         }
 
-        self.actionExit.triggered.connect(self.close_application)
-
         self.updateButton.clicked.connect(self.update_from_input)
 
         self.vertCheckButtonGroup = MyQButtonGroup(self.vertCheckBoxLayout)
 
         self.set_shortcut()
 
-    def close_application(self):
-        # this running means somewhere, an option to leave has been clicked
-        choice = qw.QMessageBox.question(self, 'Shut it Down',
-                                      'Are you sure?',
-                                      qw.QMessageBox.Yes | qw.QMessageBox.No)
-        if choice == qw.QMessageBox.Yes:
-            print('exiting program...')
-            sys.exit()
-        else:
-            pass
+    def set_dataframe(self, filename):
+        self.df = pd.read_csv(filename, header=0, index_col=0)  # read file and set header row
+        mask = self.df['NE'].notna()
+        print(self.df.shape, self.df[mask].shape)
+        self.df.loc[mask, 'alias'].fillna(self.df[mask].index.to_series(), inplace=True)
+        self.df.fillna('', inplace=True)
+        self.vocabTableWidget.print_table(self.df, self.vocab_limit)
 
-    def file_open(self):
-        """
-        GUI file picker wrapper on pandas-to-tab method
-        """
-        try:
-            fileName, _ = qw.QFileDialog.getOpenFileName(self, 'Open File')
-            # fileName = 'app_vocab.csv'
-            self.csv_to_tab(fileName)
-        except FileNotFoundError:
-            pass
-
-    def file_save(self):
-        fileName, _ = qw.QFileDialog.getSaveFileName(self, 'Save File')
-        self.df.reset_index().to_csv(fileName, index=False)
-
-    def extract_row_info(self):
+    def table_item_selected(self):
         items = self.vocabTableWidget.selectedItems()  # selected row
         tok, clf, alias, notes = (str(i.text()) for i in items)
-        # print(tok)
-        # if clf in self.clf_mapper.keys():
         if alias is not '':
             self.aliasEdit.setText(alias)  # preferred alias
         else:
             self.aliasEdit.setText(tok)  # Default to tok
-        # btn = self.clf_mapper[clf]  # which button?
         btn = self.clf_mapper.get(clf, self.unsetButton)
         btn.toggle()  # toggle that button
         self.notesTextEdit.setText(notes)  # show any notes
 
         matches = zz.extractBests(tok, self.df.index.tolist(),
-                                       limit=10, score_cutoff=self.thres)
+                                  limit=10, score_cutoff=self.thres)
 
-        self.vertCheckButtonGroup.update_checkboxes(tok, matches, self.df)
+        self.vertCheckButtonGroup.update_checkboxes(tok, matches, window.df)
 
     def update_from_input(self):
         """
@@ -153,14 +177,12 @@ class MyWindow(qw.QMainWindow, Ui_MainWindow):
         items = self.vocabTableWidget.selectedItems()  # selected row
         tok, clf, alias, notes = (str(i.text()) for i in items)
 
-        # print(self.clfButtonGroup.checkedId())
         new_alias = self.aliasEdit.text()
         new_notes = self.notesTextEdit.text()
         new_clf = self.btn_mapper.get(self.clfButtonGroup.checkedButton().text(), pd.np.nan)
 
         tok_list = [tok]
 
-        #TODO see doc QAbstractButton *QButtonGroup::checkedButton()
         for btn in self.vertCheckButtonGroup.buttons():
             s = btn.text()[4:]
             if btn.isChecked():
@@ -171,47 +193,50 @@ class MyWindow(qw.QMainWindow, Ui_MainWindow):
                     self.df.loc[s, 'alias'] = ''
                     self.df.loc[s, 'notes'] = ''
 
-        print([new_clf, new_alias, new_notes])
         self.df.loc[tok_list, 'NE'] = new_clf
         self.df.loc[tok_list, 'alias'] = new_alias
         self.df.loc[tok_list, 'notes'] = new_notes
-        self.refresh_pd_tab()
+        self.vocabTableWidget.print_table(self.df, self.vocab_limit)
         self.vocabTableWidget.setFocus()
 
         row = self.vocabTableWidget.currentRow()
         self.vocabTableWidget.selectRow(row+1)
         print(new_clf, new_alias, new_notes)
 
-    def csv_to_tab(self, filename):
-        self.df = pd.read_csv(filename, header=0, index_col=0)  # read file and set header row
-        mask = self.df['NE'].notna()
-        print(self.df.shape, self.df[mask].shape)
-        self.df.loc[mask, 'alias'].fillna(self.df[mask].index.to_series(), inplace=True)
-        self.df.fillna('', inplace=True)
-        self.refresh_pd_tab()
+        self.update_progress_bar()
 
-    def refresh_pd_tab(self):
-        temp_df = self.df.reset_index()
-        nrows, ncols = temp_df.shape
-        self.vocabTableWidget.setColumnCount(ncols)
-        self.vocabTableWidget.setRowCount(min([nrows, self.vocab_limit]))
-
-        for i in range(min([nrows, self.vocab_limit])):
-            for j in range(ncols):
-                self.vocabTableWidget.setItem(i, j, qw.QTableWidgetItem(str(temp_df.iat[i, j])))
-
-        self.vocabTableWidget.resizeColumnsToContents()
-        self.vocabTableWidget.resizeRowsToContents()
-        self.vocabTableWidget.setHorizontalHeaderLabels(temp_df.columns.tolist())
-        self.vocabTableWidget.setSelectionBehavior(qw.QTableWidget.SelectRows)
-
-        completed_pct = int(100 * self.df[self.df['NE'] != ''].shape[0] / self.df.shape[0])
-        self.progressBar.setValue(completed_pct)
+    def update_progress_bar(self):
+         completed_pct = int(100 * self.df[self.df['NE'] != ''].shape[0] / self.df.shape[0])
+         self.progressBar.setValue(completed_pct)
 
     def fuzz_thres(self):
         val = self.simthresSlider.value()
         self.thres = val
-        self.extract_row_info()
+        self.table_item_selected()
+
+    def file_open(self):
+        """
+        Open the csv file and save it as a dataframe using set_dataframe
+        """
+        try:
+            fileName, _ = Qw.QFileDialog.getOpenFileName(self, 'Open File')
+            # fileName = 'app_vocab.csv'
+            self.set_dataframe(fileName)
+            #self.csv_to_tab(fileName)
+        except FileNotFoundError:
+            pass
+
+    def file_save(self):
+        fileName, _ = Qw.QFileDialog.getSaveFileName(self, 'Save File')
+        self.df.reset_index().to_csv(fileName, index=False)
+
+    def set_menu(self):
+        self.openFile = self.actionOpen
+        self.openFile.setStatusTip('Open File')
+        self.openFile.triggered.connect(self.file_open)
+        self.saveFile = self.actionSave
+        self.saveFile.setStatusTip('Save File')
+        self.saveFile.triggered.connect(self.file_save)
 
     def set_shortcut(self):
         self.openFile.setShortcut("Ctrl+O")
@@ -223,19 +248,29 @@ class MyWindow(qw.QMainWindow, Ui_MainWindow):
         self.stpwButton.setShortcut(QCoreApplication.translate("MainWindow", "Alt+X"))
         self.unsetButton.setShortcut(QCoreApplication.translate("MainWindow", "Alt+Shift+X"))
 
-        notesShortCut = qw.QShortcut(QKeySequence("Alt+N"), self)
+        notesShortCut = Qw.QShortcut(QKeySequence("Alt+N"), self)
         notesShortCut.activated.connect(lambda: self.editing_mode(self.notesTextEdit))
 
-        aliasShortCut = qw.QShortcut(QKeySequence("Alt+A"), self)
+        aliasShortCut = Qw.QShortcut(QKeySequence("Alt+A"), self)
         aliasShortCut.activated.connect(lambda: self.editing_mode(self.aliasEdit))
 
     def editing_mode(self, field):
         field.setFocus()
         field.selectAll()
 
+    def close_application(self):
+        # this running means somewhere, an option to leave has been clicked
+        choice = Qw.QMessageBox.question(self, 'Shut it Down',
+                                      'Are you sure?',
+                                         Qw.QMessageBox.Yes | Qw.QMessageBox.No)
+        if choice == Qw.QMessageBox.Yes:
+            print('exiting program...')
+            sys.exit()
+        else:
+            pass
 
 if __name__ == "__main__":
-    app = qw.QApplication(sys.argv)
+    app = Qw.QApplication(sys.argv)
     window = MyWindow()
     window.show()
     sys.exit(app.exec_())
