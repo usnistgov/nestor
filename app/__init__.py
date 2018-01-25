06a1ace7acd146e8352e0b65b08b3c999f0ff9bf
Moccozet, Sascha A. (IntlAssoc) <saschamoccozet@gmail.com>
2018-01-25 15:16:22 -0500

    Ngrame UI  V0.0.0.0.0.0.0.0.0.1 Beta :P
    refactor  grame UI

--- app/__init__.py
+++ app/__init__.py
@@ -1,3 +1,7 @@
 from .test_skeleton import Ui_MainWindow
+from .n_grame_ui_skeleton import Ui_MainWindow
+
 from .helper_objects import MyQButtonGroup
 from .helper_objects import MyQTableWidget
+from .helper_objects import My1gTokenLayout
+
