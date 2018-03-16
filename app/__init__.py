e61b4927ab153d6fa562ed6dc8a0b0fe4a708345
Moccozet, Sascha A. (IntlAssoc) <saschamoccozet@gmail.com>
2018-03-16 12:19:09 -0400

    remove unworked stuff

--- app/__init__.py
+++
@@ -1,7 +0,0 @@
-from .test_skeleton import Ui_MainWindow
-from .n_grame_ui_skeleton import Ui_MainWindow
-
-from .helper_objects import MyQButtonGroup
-from .helper_objects import MyQTableWidget
-from .helper_objects import My1gTokenLayout
-
