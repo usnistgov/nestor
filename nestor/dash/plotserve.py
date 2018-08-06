13a2e7db97018cb68db3ccd3521541c270d40c96
tbsexton <tbsexton@asu.edu>
2018-08-06 16:29:05 -0400

    port problems again

--- nestor/dash/plotserve.py
+++ nestor/dash/plotserve.py
@@ -24,7 +24,13 @@ def serve_bokeh_tags(hdfstore):
         '/node_mach': renderer.app(tagplot.hv_nodelink('mach').options(width=600, height=600)),
         '/flow_tech': renderer.app(tagplot.hv_flow('tech').options(width=900, height=600)),
         '/flow_mach': renderer.app(tagplot.hv_flow('mach').options(width=900, height=600)),
-    }, port=5006, allow_websocket_origin=["127.0.0.1:5000", "localhost:5000", "localhost:5006"])
+    }, port=5006, allow_websocket_origin=[
+        "127.0.0.1:5000", 
+        "localhost:5000", 
+        "localhost:5006", 
+        "0.0.0.0:5000", 
+        "0.0.0.0:5006"                                     
+    ])
     server.start()
     # server.show('/')
     loop = IOLoop.current()
