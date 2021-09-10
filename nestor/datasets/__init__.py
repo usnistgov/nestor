4b1b2619573d64d70d8c1a672cdd87b92680b7ab
Thurston Sexton <thurston.sexton@nist.gov>
2021-09-10 15:15:14 -0400

    new way to get vocabs

--- nestor/datasets/__init__.py
+++ nestor/datasets/__init__.py
@@ -1,3 +1,4 @@
 from .excavators import load_excavators
+from .vocab import load_vocab
 
 __all__ = ["load_excavators", "load_vocab"]
--- nestor/datasets/vocab.py
+++ nestor/datasets/vocab.py
@@ -29,15 +29,72 @@ class VocabOption(str, Enum):
     def filename(self, ngram: bool = False) -> str:
         """template for consistent vocab access"""
         ng = 2 if ngram else 1
-        return "vocab-{}-{}.csv".format(self, ng)
+        return "vocab-{}-{}g.csv".format(self, ng)
 
     def location(self, ngram: bool = False, remote: bool = False) -> Path:
         def location_local() -> Path:
             module_path = Path(__file__).parent
             return module_path / "resources" / self.filename(ngram)
 
-        def location_gitlab() -> Path:
-            return
+        def location_gitlab() -> str:
 
-        def location_github() -> Path:
-            return
+            url = (
+                "https://gitlab.nist.gov/gitlab/kea/nestor-suite/nestor/-/raw/"
+                "bio-vocab/nestor/datasets/resources/{}"
+            ).format(self.filename(ngram))
+            return url
+
+        def location_github() -> str:
+            raise NotImplementedError("Not pushed to github yet!")
+            return "NOT IMPLEMENTED"
+
+        if not remote:
+            return location_local()
+
+        else:
+            try:
+                url = location_github()
+            except NotImplementedError:
+                from warnings import warn
+
+                warn("GitHub Download not implemented yet!")
+                url = location_gitlab()
+            finally:
+                # TODO: should we allow cache overwrites?
+                return download_datafile(url, self.filename(ngram))
+
+
+def load_vocab(
+    vocab_option: VocabOption,
+    base1g: bool = True,
+    compound2g: bool = True,
+    remote=False,
+) -> pd.DataFrame:
+    """convenience function to load provided nestor-compatible vocabulary files
+
+    for a list of currently-provided options, see the [`VocabOptions` class](nestor.datasets.vocab.VocabOptions)
+
+    """
+
+    vocab_option = VocabOption[
+        vocab_option
+    ]  # coerce, allowing users to pass vanilla str
+
+    def load_vocab_csv(filename: Path) -> pd.DataFrame:
+        return pd.read_csv(filename, index_col=0)
+
+    if not (base1g or compound2g):
+        from warnings import warn
+
+        warn("Must at least choose one vocab type... defaulting to 1g!")
+
+    if compound2g:
+        vocab2g = load_vocab_csv(vocab_option.location(ngram=True, remote=remote))
+
+        if base1g:
+            vocab = load_vocab_csv(vocab_option.location(ngram=False, remote=remote))
+            return pd.concat([vocab, vocab2g])
+        else:
+            return vocab2g
+    else:  # assume 1g
+        return load_vocab_csv(vocab_option.location(ngram=False, remote=remote))
