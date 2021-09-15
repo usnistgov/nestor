506a2155aaeec6b7e23a3de3382b91f425f1aae2
Thurston Sexton <thurston.sexton@nist.gov>
2021-09-15 17:15:06 -0400

    try mv to nist-pages?

--- nist-pages-deploy.sh
+++ nist-pages-deploy.sh
@@ -5,10 +5,11 @@ STATUS="$(git status)"
 
 if [[ $STATUS == *"nothing to commit, working tree clean"* ]]
 then
-    sed -i "" '/public/' ./.gitignore
+    # sed -i "" '/public/' ./.gitignore
+    mv public nist-pages
     git add .
     git commit -m "Edit .gitignore to publish"
-    git push nist-origin `git subtree split --prefix public master`:nist-pages --force
+    git push nist-origin `git subtree split --prefix nist-pages master`:nist-pages --force
     git reset HEAD~
     git checkout .gitignore
 else
