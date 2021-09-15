db946af336ceb521ff88d999f48bb1d533986f7d
Thurston Sexton <thurston.sexton@nist.gov>
2021-09-15 17:04:01 -0400

    nist-origin

--- nist-pages-deploy.sh
+++ nist-pages-deploy.sh
@@ -1,3 +1,4 @@
+#!/bin/bash
 # adapted from https://stackoverflow.com/a/40178818 (Thanks @Matt Zeunert)
 
 STATUS="$(git status)"
@@ -7,7 +8,7 @@ then
     sed -i "" '/public/' ./.gitignore
     git add .
     git commit -m "Edit .gitignore to publish"
-    git push origin `git subtree split --prefix public master`:nist-pages --force
+    git push nist-origin `git subtree split --prefix public master`:nist-pages --force
     git reset HEAD~
     git checkout .gitignore
 else
