acc7a988ca1e9a6977daee79a1bf616333f2bd0d
Thurston Sexton <thurston.sexton@nist.gov>
2021-09-15 16:57:11 -0400

    maybe auto github deploy from gitlab

---
+++ nist-pages-deploy.sh
@@ -0,0 +1,15 @@
+# adapted from https://stackoverflow.com/a/40178818 (Thanks @Matt Zeunert)
+
+STATUS="$(git status)"
+
+if [[ $STATUS == *"nothing to commit, working tree clean"* ]]
+then
+    sed -i "" '/public/' ./.gitignore
+    git add .
+    git commit -m "Edit .gitignore to publish"
+    git push origin `git subtree split --prefix public master`:nist-pages --force
+    git reset HEAD~
+    git checkout .gitignore
+else
+    echo "Need clean working directory to publish"
+fi
